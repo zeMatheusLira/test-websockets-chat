e0967b68bde8acdd4fce264557f33d33c90bc316
Zé Matheus <josemw86@gmail.com>
2024-09-15 22:24:17 -0300

    commit initial

---
+++ src/main/java/chat/testwebcocketschat/configs/TestWebcocketsChatApplication.java
@@ -0,0 +1,13 @@
+package chat.testwebcocketschat.configs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebcocketsChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebcocketsChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chat/testwebcocketschat/TestWebcocketsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.testwebcocketschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebcocketsChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
