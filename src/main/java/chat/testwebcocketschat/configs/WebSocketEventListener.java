7b93dca566f52703cac7b1c63a64cfaedbbb780f
Zé Matheus <josemw86@gmail.com>
2024-09-15 23:45:13 -0300

    WebSocket session disconnection events

--- src/main/java/chat/testwebcocketschat/configs/WebSocketEventListener.java
+++ src/main/java/chat/testwebcocketschat/configs/WebSocketEventListener.java
@@ -1,8 +1,13 @@
 package chat.testwebcocketschat.configs;
 
+import chat.testwebcocketschat.chat.ChatMessage;
+import chat.testwebcocketschat.chat.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,9 +15,22 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messageTemplate;
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info(" User disconnected: {}",  username);
+            var chatMessage = ChatMessage.builder().type(MessageType.LEAVER).sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+
+        }
+
+
 
     }
 }
