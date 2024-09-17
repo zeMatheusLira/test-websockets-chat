a4743676454363f92fe8615f7592c905a1b6e9a6
Zé Matheus <josemw86@gmail.com>
2024-09-17 12:40:14 -0300

    Update ChatController.java

--- src/main/java/chat/testwebcocketschat/chat/ChatController.java
+++ src/main/java/chat/testwebcocketschat/chat/ChatController.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
-    @SendTo("topic/public")
+    @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         return chatMessage;
     }
 
     @MessageMapping("/chat.addUser")
-    @SendTo("topic/public")
+    @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // adiciona o username na sessão do websocket
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
